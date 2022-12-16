2805548b426a0b5c1d84258c12a2d3330cfe24a2
khoi03 <khoivtvn002@gmail.com>
2022-12-16 13:34:23 +0700

    Edit file CSV and database

--- database.py
+++ database.py
@@ -12,19 +12,16 @@ deta = Deta(DETA_KEY)
 
 db = deta.Base("rec_apartments")
 
-"""def insert_apartments(names,addresses,areas,rates,utilities):
+def insert_apartments(names,links,districts,wards,addresses,areas,bedrooms,wc,rates,schools,markets,entertainment,hospitals,restaurants,buses,atm):
     
-    return db.put({"key":names, "addresses":addresses, "areas":areas, "rates":rates, "utilities": utilities})"""
-def insert_apartments(names,districts,wards,addresses,areas,bedrooms,wc,rates,schools,markets,entertainment,hospitals,restaurants,buses,atm):
-    
-    return db.put({"key":names, "districts":districts, "wards":wards, "addresses":addresses, "areas":areas, "bedrooms":bedrooms, "wc":wc,
+    return db.put({"key":names, "links":links, "districts":districts, "wards":wards, "addresses":addresses, "areas":areas, "bedrooms":bedrooms, "wc":wc,
                    "rates":rates, "schools":schools, "markets":markets, "entertainment":entertainment, "hospitals":hospitals, "restaurants":restaurants,
                    "buses":buses, "atm":atm})
                    
 
-def insert_all(names,districts,wards,addresses,areas,bedrooms,wc,rates,schools,markets,entertainment,hospitals,restaurants,buses,atm):
-    for (name,district,ward,address,area,bedroom,w,rate,school,market,entertain,hospital,restaurant,bus,at) in zip (names,districts,wards,addresses,areas,bedrooms,wc,rates,schools,markets,entertainment,hospitals,restaurants,buses,atm):
-        insert_apartments(name,district,ward,address,area,bedroom,w,rate,school,market,entertain,hospital,restaurant,bus,at)
+def insert_all(names,links,districts,wards,addresses,areas,bedrooms,wc,rates,schools,markets,entertainment,hospitals,restaurants,buses,atm):
+    for (name,link,district,ward,address,area,bedroom,w,rate,school,market,entertain,hospital,restaurant,bus,at) in zip (names,links,districts,wards,addresses,areas,bedrooms,wc,rates,schools,markets,entertainment,hospitals,restaurants,buses,atm):
+        insert_apartments(name,link,district,ward,address,area,bedroom,w,rate,school,market,entertain,hospital,restaurant,bus,at)
 
 def fetch_all_apartments():
     
@@ -40,20 +37,21 @@ df = np.array(df)
 
 #
 names = list(df[:,0])
-districts = list(df[:,1])
-wards = list(df[:,2])
-addresses = list(df[:,3])
-areas = list(df[:,4])
-bedrooms = list(df[:,5])
-wc = list(df[:,6])
-rates = list(df[:,7])
-schools = list(df[:,8])
-markets = list(df[:,9])
-entertainment = list(df[:,10])
-hospitals = list(df[:,11]) 
-restaurants = list(df[:,12])
-buses = list(df[:,13]) 
-atm = list(df[:,14])
+links = list(df[:,1])
+districts = list(df[:,2])
+wards = list(df[:,3])
+addresses = list(df[:,4])
+areas = list(df[:,5])
+bedrooms = list(df[:,6])
+wc = list(df[:,7])
+rates = list(df[:,8])
+schools = list(df[:,9])
+markets = list(df[:,10])
+entertainment = list(df[:,11])
+hospitals = list(df[:,12]) 
+restaurants = list(df[:,13])
+buses = list(df[:,14]) 
+atm = list(df[:,15])
 
 #
-insert_all(names,districts,wards,addresses,areas,bedrooms,wc,rates,schools,markets,entertainment,hospitals,restaurants,buses,atm)
+insert_all(names,links,districts,wards,addresses,areas,bedrooms,wc,rates,schools,markets,entertainment,hospitals,restaurants,buses,atm)
