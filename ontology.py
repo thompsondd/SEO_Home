ffb50d1a0605dd8eeba30d518282a6c389a6747c
Dang Laptop <dnng2002@gmail.com>
2022-12-23 13:13:27 +0700

    Syn local Dang Lap

---
+++ ontology.py
@@ -0,0 +1,14 @@
+
+class ToaDo:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
+class DiaChi:
+    def __init__(self,SoNha,Phuong_xa,Quan_huyen, ToaDo):
+        self.q=Quan_huyen
+        self.s=SoNha
+        self.p=Phuong_xa
+        self.toado = ToaDo
+
+class a:pass
